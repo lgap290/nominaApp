d54a815da60a0361e7e1173c9b3ce963c8d8997e
Gabriel Avella <luis.avellap@gmail.com>
2018-09-28 13:32:14 -0500

    Creación Proyecto

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,93 @@
+<aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/profile-image.png" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                            <p>Admin</p>
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><i class="material-icons">settings_input_svideo</i>Dashboard</a></li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">aplicaciones</i>Departamento<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="adddepartment.php">Agregar departamento</a></li>
+                                <li><a href="managedepartments.php">Administrar departamento</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey">Novedades<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addleavetype.php">Agregar Novedades</a></li>
+                                <li><a href="manageleavetype.php">Administrar Novedades</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>Empleados<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addemployee.php">Agregar empleado</a></li>
+                                <li><a href="manageemployee.php">Administrar Empleado</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+                    <!-- <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Dejar la gestión<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="leaves.php">Todas las hojas </a></li>
+                                <li><a href="pending-leavehistory.php"> Hojas pendientes </a></li>
+                                <li><a href="approvedleave-history.php">Hojas aprobadas</a></li>
+                                  <li><a href="notapproved-leaves.php">Hojas no aprobadas</a></li>
+       
+                            </ul>
+                        </div>
+                    </li> -->
+
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Liquidación<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="#">Liquidados</a></li>
+                                <li><a href="#">Agregar Liquidación </a></li>
+                                <li><a href="#">Editar Liquidación</a></li>
+                            </ul>
+                        </div>
+                    </li>
+
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>Administrador<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="#">Crear Reporte</a></li>
+                                <li><a href="#">Cambiar Clave</a></li>
+                                <li><a href="#">Crear Administrador</a></li>
+                            </ul>
+                        </div>
+                    </li>
+
+
+
+
+                        <li class="no-padding">
+                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Desconectar</a>
+                            </li>  
+                 
+
+                 
+              
+                </ul>
+                <div class="footer">
+                
+                </div>
+                </div>
+            </aside>
